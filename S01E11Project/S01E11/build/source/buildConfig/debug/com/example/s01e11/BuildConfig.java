50c946ec7ffad27b3bd083c078839c4a801d7f0e
godvmxi <godvmxi@gmail.com>
2013-08-24 18:23:25 +0800

    add some demos

---
+++ S01E11Project/S01E11/build/source/buildConfig/debug/com/example/s01e11/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.s01e11;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ S01E12Project/S01E12/build/source/buildConfig/debug/com/example/s01e12/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.s01e12;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
